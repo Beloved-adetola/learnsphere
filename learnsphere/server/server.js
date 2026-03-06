c4eea26458533aceefc2a737f205e8d67f3fd0cb
Adetola Beloved <belovedadetola@gmail.com>
2026-03-07 00:56:33 +0100

    update cors setup again

--- learnsphere/server/server.js
+++ learnsphere/server/server.js
@@ -64,8 +64,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.options('/{*splat}', cors(corsOptions));
 // Middleware
 app.use(express.json());
 
