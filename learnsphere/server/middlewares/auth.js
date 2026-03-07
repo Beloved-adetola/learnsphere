ccf1e8f2a07caf656d39236ab6bf795db8d4c36e
Adetola Beloved <belovedadetola@gmail.com>
2026-03-07 01:06:14 +0100

    update auth middleware

--- learnsphere/server/middlewares/auth.js
+++ learnsphere/server/middlewares/auth.js
@@ -2,9 +2,17 @@ import admin from "firebase-admin";
 import User from "../models/User.js";
 
 export async function authenticate(req, res, next) {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
   const authHeader = req.header("Authorization");
-  const idToken = authHeader?.split("Bearer ")[1] || authHeader?.split("Bearer")[1];
-  if (!idToken) return res.status(401).send("No token provided");
+  const idToken =
+    authHeader?.split("Bearer ")[1] || authHeader?.split("Bearer")[1];
+
+  if (!idToken) {
+    return res.status(401).send("No token provided");
+  }
 
   try {
     const decoded = await admin.auth().verifyIdToken(idToken);
@@ -12,14 +20,14 @@ export async function authenticate(req, res, next) {
     req.email = decoded.email;
 
     const user = await User.findOne({ uid: decoded.uid });
-    
-    if (!user && !req.originalUrl.includes('/register')) {
+
+    if (!user && !req.originalUrl.includes("/register")) {
       return res.status(404).send("No user profile found");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    res.status(401).json({ error: "Invalid token" });
+    return res.status(401).json({ error: "Invalid token" });
   }
 }
